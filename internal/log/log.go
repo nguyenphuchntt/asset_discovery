41cf696ad64fabf80fa1a97181c5c1ac4f77d29a
Nguyen Van Phuc <nguyenvanphucuet@gmail.com>
2026-06-22 10:56:39 +0700

    feat: add basic logger

---
+++ internal/log/log.go
@@ -0,0 +1,38 @@
+package log
+
+import (
+	"strings"
+	"fmt"
+	"log/slog"
+	"os"
+)
+
+func New(level string) (*slog.Logger, error) {
+	level = strings.ToLower(strings.TrimSpace(level))
+
+	slogLevel, err := parseLevel(level)
+	if err != nil {
+		return nil, err
+	}
+
+	handler := slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		Level: slogLevel,
+	})
+
+	return slog.New(handler), nil
+}
+
+func parseLevel(level string) (slog.Level, error) {
+	switch level {
+	case "debug":
+		return slog.LevelDebug, nil
+	case "info":
+		return slog.LevelInfo, nil
+	case "warn":
+		return slog.LevelWarn, nil
+	case "error":
+		return slog.LevelError, nil
+	default:
+		return slog.LevelInfo, fmt.Errorf("invalid --log-level %q", level)
+	}
+}
